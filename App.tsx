60c5ae534502b8b748c66851498500e1de8551d2
alanCordovaQuijij <alanquijije@gmail.com>
2024-02-05 06:54:51 -0500

    fix: cambios

--- App.tsx
+++ App.tsx
@@ -1,63 +1,57 @@
 import React from 'react';
-import { View, TextInput, Button, Text } from 'react-native';
-import { useForm, Controller, SubmitHandler, SubmitErrorHandler } from 'react-hook-form';
+import {View, TextInput, Button, Text} from 'react-native';
+import {
+  useForm,
+  Controller,
+  SubmitHandler,
+  SubmitErrorHandler,
+} from 'react-hook-form';
 import * as yup from 'yup';
-import { Formulario } from './formulario';
+import {Formulario} from './formulario';
 
 // Definir el esquema de validación con Yup
 const validationSchema = yup.object().shape({
   name: yup.string().required('Este campo es obligatorio'),
-  email: yup.string().email('Ingrese una dirección de correo electrónico válida').required('Este campo es obligatorio'),
-  password: yup.string().min(6, 'La contraseña debe tener al menos 6 caracteres').required('Este campo es obligatorio'),
+  email: yup
+    .string()
+    .email('Ingrese una dirección de correo electrónico válida')
+    .required('Este campo es obligatorio'),
+  password: yup
+    .string()
+    .min(6, 'La contraseña debe tener al menos 6 caracteres')
+    .required('Este campo es obligatorio'),
+  codItem: yup.string().required('Campo es obligatorio'),
 });
 
 // Definir el tipo de datos del formulario
-interface FormInputs {
+export interface FormInputs {
   name: string;
   email: string;
   password: string;
+  codItem: string;
 }
 
 const App: React.FC = () => {
   const {
     control,
     handleSubmit,
-    formState: { errors },
-  } = useForm<FormInputs>({
-    resolver: async (data) => {
-      try {
-        await validationSchema.validate(data, { abortEarly: false });
-        return { values: data, errors: {} };
-      } catch (err) {
-        return { values: {}, errors: yupToFormErrors(err as unknown as any) };
-      }
-    },
-  });
+    setValue,
+    getValues,
+    formState: {errors},
+  } = useForm<FormInputs>();
 
-  const yupToFormErrors = (yupError: yup.ValidationError) => {
-    const convertedErrors: Record<string, string> = {};
-    for (const error of yupError.inner) {
-      convertedErrors[error.path as string] = error.message;
-    }
-    return convertedErrors;
-  };
-
-  const onSubmit: SubmitHandler<FormInputs> = (data) => {
+  const onSubmit = (data: FormInputs) => {
     // Aquí puedes manejar la lógica cuando el formulario es enviado
-    console.log("DATA DEL FORMULARIO",data);
-  };
+    console.log('[DATA DEL FORMULARIO]======>', data);
+    console.log('[DATA DEL FORMULARIO]======>', getValues('name'));
 
-  const onError: SubmitErrorHandler<FormInputs> = (errors, event) => {
-    // Manejar errores (opcional)
-    console.error(errors);
   };
 
   return (
     <View>
+      <Formulario control={control} errors={errors} setValue={setValue} />
 
-      <Formulario control={control} errors={errors}/>
-
-      <Button title="Enviar" onPress={handleSubmit(onSubmit, onError)} />
+      <Button title="Enviar" onPress={handleSubmit(onSubmit)} />
     </View>
   );
 };
--- formulario.tsx
+++ formulario.tsx
@@ -1,15 +1,39 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
-import { Controller } from 'react-hook-form'
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Control,
+  Controller,
+  FieldErrors,
+  UseFormSetValue,
+} from 'react-hook-form';
+import {Dropdown} from 'react-native-element-dropdown';
+import {FormInputs} from './App';
 
+const data = [
+  {label: 'Item 1', value: '1'},
+  {label: 'Item 2', value: '2'},
+  {label: 'Item 3', value: '3'},
+  {label: 'Item 4', value: '4'},
+  {label: 'Item 5', value: '5'},
+  {label: 'Item 6', value: '6'},
+  {label: 'Item 7', value: '7'},
+  {label: 'Item 8', value: '8'},
+];
 
+interface Iprops {
+  contol: Control;
+  errors: FieldErrors;
+  setValue: UseFormSetValue<FormInputs>;
+}
 
-export  const  Formulario = ({control, errors}: any) => {
+export const Formulario = ({control, errors, setValue}: any) => {
+  const [codItem, setCodItem] = useState(null);
+
   return (
     <View>
-            <Controller
+      <Controller
         control={control}
-        render={({ field }) => (
+        render={({field}) => (
           <View>
             <TextInput
               placeholder="Nombre"
@@ -17,7 +41,7 @@ export  const  Formulario = ({control, errors}: any) => {
               onBlur={field.onBlur}
               value={field.value}
             />
-            <Text style={{ color: 'red' }}>{errors.name?.message}</Text>
+            <Text style={{color: 'red'}}>{errors.name?.message}</Text>
           </View>
         )}
         name="name"
@@ -25,15 +49,15 @@ export  const  Formulario = ({control, errors}: any) => {
 
       <Controller
         control={control}
-        render={({ field }) => (
+        render={({field: {value, onBlur, onChange}}) => (
           <View>
             <TextInput
               placeholder="Correo electrónico"
-              onChangeText={field.onChange}
-              onBlur={field.onBlur}
-              value={field.value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
             />
-            <Text style={{ color: 'red' }}>{errors.email?.message}</Text>
+            <Text style={{color: 'red'}}>{errors.email?.message}</Text>
           </View>
         )}
         name="email"
@@ -41,22 +65,93 @@ export  const  Formulario = ({control, errors}: any) => {
 
       <Controller
         control={control}
-        render={({ field }) => (
+        render={({field: {onBlur, value, onChange}}) => (
           <View>
             <TextInput
               placeholder="Contraseña"
-              onChangeText={field.onChange}
-              onBlur={field.onBlur}
-              value={field.value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
               secureTextEntry
             />
-            <Text style={{ color: 'red' }}>{errors.password?.message}</Text>
+            <Text style={{color: 'red'}}>{errors.password?.message}</Text>
           </View>
         )}
         name="password"
       />
-    </View>
-  )
-}
 
-const styles = StyleSheet.create({})
+      {/*       <Controller
+        control={control}
+        render={({field: {onBlur, value, onChange}}) => (
+          <Dropdown
+            style={styles.dropdown}
+            placeholderStyle={styles.placeholderStyle}
+            selectedTextStyle={styles.selectedTextStyle}
+            inputSearchStyle={styles.inputSearchStyle}
+            iconStyle={styles.iconStyle}
+            data={data}
+            maxHeight={300}
+            labelField="label"
+            valueField="value"
+            placeholder="Select item"
+            searchPlaceholder="Search..."
+           // value={value}
+            onChange={(e) => {
+              setValue('codItem', e.value)
+            }
+            }
+          />
+        )}
+        name='codItem'
+      /> */}
+
+   
+          <Dropdown
+            style={styles.dropdown}
+            placeholderStyle={styles.placeholderStyle}
+            selectedTextStyle={styles.selectedTextStyle}
+            inputSearchStyle={styles.inputSearchStyle}
+            iconStyle={styles.iconStyle}
+            data={data}
+            maxHeight={300}
+            labelField="label"
+            valueField="value"
+            placeholder="Select item"
+            searchPlaceholder="Search..."
+            // value={value}
+            onChange={e => {
+              setValue('codItem', e.value);
+            }}
+          />
+        
+   
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  dropdown: {
+    margin: 16,
+    height: 50,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 0.5,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
